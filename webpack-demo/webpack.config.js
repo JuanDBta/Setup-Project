30ec3711f636c21249030919c0b46bf095c384e3
JuanDBta <juandavid_diaz@hotmail.com>
2023-05-09 18:42:42 -0500

    CSS done

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,19 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js',
-      },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-      ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+ module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+     },
+   ],
+ },
 };
